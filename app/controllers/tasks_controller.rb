e8b8b141ff78e269f859b8049f5e3ad46daf8945
shinnosuke.yayama <yymsnsk@gmail.com>
2021-01-23 08:42:49 +0000

    change the user relating

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,28 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user,only: [:destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+    # @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+    # ログインユーザのタスクだけ表示するように変更
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
   end
   def show
     #@task = Task.find(params[:id])
   end
   def new
-    @task = Task.new
+    # @task = Task.new
+    # userインスタンスを紐づけるフォームを用意するように変更
+    @task = current_user.tasks.build # form_with用
   end
   def create
-    @task = Task.new(task_params)
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に登録されました'
-      redirect_to @root_pah
+      redirect_to root_url
     else
       flash.now[:danger] = 'Taskが登録されませんでした'
       render :new
@@ -40,7 +46,7 @@ class TasksController < ApplicationController
     # @task = Task.find(params[:id])
     @task.destroy
     
-    flash[:success] = 'Taskは正常に削除されました'
+    flash[:success] = 'Taskは正常に削除されました。'
     redirect_to tasks_url
   end
 
@@ -53,6 +59,14 @@ class TasksController < ApplicationController
   # Strong Patameter
   def task_params
     params.require(:task).permit(:content,:status)
+  end
+  
+  # before destroy , checking user belonging.
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
---
+++ db/migrate/20210123063101_user_to_tasks.rb
@@ -0,0 +1,4 @@
+class UserToTasks < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
